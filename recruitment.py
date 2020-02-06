b03958a50529d168753fba0ecccb8e2e657f353f
Essam <essamabuissa@hotmail.com>
2020-02-06 11:08:46 +0200

    Recruitment Code Done

--- recruitment.py
+++ recruitment.py
@@ -1,5 +1,26 @@
 def main():
-	#write your code here
+	skills = ["Python", "C++", "JavaScript", "Meeting", "Leeting", "Eating"]
+	cv = {"name":"","age":0,"experience":0}
+	print("Welcome to the special recruitment program, please answer the following questions:")
+	cv['name'] = input("What\'s your name? ")
+	cv['age']  = int(input("How old are you? "))
+	cv['experience'] = int(input("How many years of experience do you have? "))
+	cv['skills'] = []
+	print()
+	counter = 1
+	for skill in skills:
+		print(f"{counter}.{skill}")
+		counter += 1
+	print()
+	choose_skill = int(input("Choose a skill from above by entering its number: "))
+	cv['skills'].append(skills[choose_skill -1])
+	choose_skill = int(input("Choose another skill from above by entering its number: "))
+	cv['skills'].append(skills[choose_skill -1])
+	if cv['age'] >= 25 and cv['age'] <= 40 and cv['experience'] > 5 and skills[5] in cv['skills']:
+		print("Congrats you are accepted")
+	else:
+		print("Sorry you are rejected")
+
 
 if __name__ == '__main__':
 	main()
